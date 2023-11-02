81613032ed3aac82e311e2ab2149971c18c813be
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2023-11-02 19:12:35 +0000

    downloading models

--- utils.py
+++ utils.py
@@ -2,6 +2,28 @@
 
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from datasets import load_dataset
+import os
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-70b-chat-hf")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-70b-chat-hf", device_map="auto")
+def load_and_save_model_from_hub(model_name:str, save_dir:str="models/") -> (AutoTokenizer, AutoModelForCausalLM):
+    """This function downloads functions from huggingface and save them locally,
+    for llama models you will need a token that proves you have a licence to download """
+    print("model name: ", model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
+    model.save_pretrained(save_dir+model_name, from_pt=True)
+
+    return model, tokenizer
+
+
+
+models = ["meta-llama/Llama-2-70b-chat-hf",
+"meta-llama/Llama-2-70b-hf",
+"meta-llama/Llama-2-13b-chat-hf",
+"meta-llama/Llama-2-13b-hf",
+"meta-llama/Llama-2-7b-chat-hf",
+"meta-llama/Llama-2-7b-hf",
+]
+for model_name in models:
+    model, tokenizer = load_and_save_model_from_hub(model_name)
+
