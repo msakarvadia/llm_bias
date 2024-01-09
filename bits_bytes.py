5f2066989198935f5607b5da4d3b1ce953455de3
Mansi Sakarvadia <sakarvadia@uchicago.edu>
2024-01-09 15:21:57 -0600

    working on inference pipeline

--- bits_bytes.py
+++ bits_bytes.py
@@ -1,27 +1,47 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+def assess_device_memory():
+    free_in_GB = int(torch.cuda.mem_get_info()[0]/1024**3)
+    print("free GB:", free_in_GB)
+
+def load_model_w_bits_and_bytes(model_name):
+    model = AutoModelForCausalLM.from_pretrained(
+      model_name,
+      device_map='auto',
+      load_in_4bit=True,
+      max_memory=max_memory
+    )
+    return model
+
+def load_model_w_HF_device_map(model_name):
+    #TODO
+    
+    return model
+
 MAX_NEW_TOKENS = 128
 model_name = "mistralai/Mistral-7B-v0.1"
+model_name = "meta-llama/Llama-2-7b-chat-hf"
+model_name = "meta-llama/Llama-2-13b-chat-hf"
+model_name = "meta-llama/Llama-2-70b-chat-hf"
 
 text = 'Hamburg is in which country?\n'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-input_ids = tokenizer(text, return_tensors="pt").input_ids
+device = "cuda"
+input_ids = tokenizer(text, return_tensors="pt").input_ids.to(device)
 
-free_in_GB = int(torch.cuda.mem_get_info()[0]/1024**3)
+assess_device_memory()
 max_memory = f'{int(torch.cuda.mem_get_info()[0]/1024**3)-2}GB'
-print("free GB:", free_in_GB)
 print("max mem:", max_memory)
 
 n_gpus = torch.cuda.device_count()
 max_memory = {i: max_memory for i in range(n_gpus)}
 
-model = AutoModelForCausalLM.from_pretrained(
-  model_name,
-  device_map='auto',
-  load_in_4bit=True,
-  max_memory=max_memory
-)
+
+load_model_w_bits_and_bytes(model_name):
+
+print("Loaded model into memory")
+assess_device_memory()
 
 param_size = 0
 for param in model.parameters():
@@ -34,7 +54,10 @@ size_all_mb = (param_size + buffer_size) / 1024**2
 print('model size: {:.3f}MB'.format(size_all_mb))
 
 generated_ids = model.generate(input_ids, max_length=MAX_NEW_TOKENS)
+print("This is the model's input text: ", text)
+print("This is the model's response: ")
 print(tokenizer.decode(generated_ids[0], skip_special_tokens=True))
+
 
 
 
