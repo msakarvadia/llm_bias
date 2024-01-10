8d39e7ac6e8803acab49d6d8231553fcc959bf50
Mansi Sakarvadia <sakarvadia@uchicago.edu>
2024-01-10 13:23:07 -0600

    created data to do inference w/

--- data/preprocess_data.py
+++ data/preprocess_data.py
@@ -1,14 +1,32 @@
 """This file preprocesses exploratory data."""
 
-import pandas
+import pandas as pd
 
-def load_data_from_csv(filename:str) -> pandas.DataFrame:
+def load_data_from_csv(filename:str) -> pd.DataFrame:
     """ Load data from csv and return pandas df"""
-    #TODO
+    return pd.read_csv(filename)
 
-    return 0
-
-def save_data_as_csv(filename:str):
+def save_data_as_csv(df: pd.DataFrame,filename:str):
     """Save data from pandas df to csv"""
+    return df.to_csv(filename)
 
-    return
+def create_exploratory_inference_data(input_filename:str="bias_data.csv", output_filename:str="inference.csv"):
+    data = load_data_from_csv(input_filename)
+
+    categories= ['gender', 'education', 'race', 'geography', 'politics']
+    
+    inference = pd.DataFrame(columns = ['prompt', 'category'])
+
+    for q in data['questions']:
+        for c in categories:
+            for pre in data[c].dropna():
+                prompt = pre+" "+q
+                row = {'prompt':prompt,
+                       'category':c}
+                inference.loc[len(inference)] = row
+                #print(pre+" "+q)
+    #print(inference.head())
+    save_data_as_csv(df=inference, filename="inference.csv")
+
+if __name__=="__main__":
+    create_exploratory_inference_data("bias_data.csv")
