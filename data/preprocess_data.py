210f889a158b41052e3ec29b581a40e0a0c8c7c8
Mansi Sakarvadia <sakarvadia@uchicago.edu>
2024-01-10 11:02:10 -0600

    added pandas dependancy to requirments.txt

--- data/preprocess_data.py
+++ data/preprocess_data.py
@@ -1,2 +1,14 @@
 """This file preprocesses exploratory data."""
 
+import pandas
+
+def load_data_from_csv(filename:str) -> pandas.DataFrame:
+    """ Load data from csv and return pandas df"""
+    #TODO
+
+    return 0
+
+def save_data_as_csv(filename:str):
+    """Save data from pandas df to csv"""
+
+    return
