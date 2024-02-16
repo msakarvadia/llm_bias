0671dcc82fb07213e77ca077498ab26d4e5db55f
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2024-02-16 21:34:36 +0000

    adding scratch work

--- scratch_work/llama2_lens.py
+++ scratch_work/llama2_lens.py
@@ -18,9 +18,13 @@ models = [
 #"meta-llama/Llama-2-70b-chat-hf",
 #"meta-llama/Llama-2-70b-hf",
 #"meta-llama/Llama-2-7b-chat-hf",
-"meta-llama/Llama-2-7b-hf",
+"EleutherAI/gpt-j-6B",
+#"meta-llama/Llama-2-7b-hf",
 #"meta-llama/Llama-2-13b-chat-hf",
-"meta-llama/Llama-2-13b-hf",
+#"meta-llama/Llama-2-13b-hf",
+"gpt-neo-125M",
+"EleutherAI/gpt-neo-1.3B",
+"EleutherAI/gpt-neo-2.7B",
 ]
 
 import torch
@@ -36,6 +40,7 @@ if __name__=="__main__":
         #Load model, tokenizer
         assess_device_memory()
 
+        '''
         tokenizer = LlamaTokenizer.from_pretrained(model_name)
         hf_model = LlamaForCausalLM.from_pretrained(model_name, 
                                                     low_cpu_mem_usage=True,
@@ -44,10 +49,14 @@ if __name__=="__main__":
 
         #Load model into TransformerLens template
         model = HookedTransformer.from_pretrained(model_name, hf_model=hf_model, tokenizer=tokenizer, device="cpu", fold_ln=False, center_writing_weights=False, center_unembed=False)
-        print(model)
+        '''
 
+        model = HookedTransformer.from_pretrained(model_name, device="cuda")
+        print(model)
         #model = model.to("cuda" if torch.cuda.is_available() else "cpu")
         #print("model moved to GPU")
         assess_device_memory()
-        output = model.generate("The capital of Germany is", max_new_tokens=20, temperature=0)
+        prompt = "disabled. BCG.com will work better for you if you enable JavaScript or switch to a JavaScript supported browser. Boston Consulting Group is an Equal Opportunity Employer. All qualified applicants will receive consideration for employment without regard to race, color,"
+        prompt = "use this file except in compliance with the License. * You may obtain a copy of the License at * http://www.apache.org/licenses/LICENSE-2.0 * Unless required by applicable law or agreed"
+        output = model.generate(prompt, max_new_tokens=50, temperature=0)
         print(output)
